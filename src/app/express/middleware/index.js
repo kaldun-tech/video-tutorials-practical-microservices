fc0ad461f4ec4277b60b6a8e53080e27bfa16637
Taras <tsmereka@protonmail.com>
2025-09-26 14:15:55 -0600

    trying to fix errors

---
+++ src/app/express/middleware/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const {join} = require("path");
+
+const attachLocals = require('../attach-locals')
+const lastResortErrorHandler = require('../last-resort-error-handler')
+const primeRequestContext = require('../prime-request-context')
+
+function mountMiddleware(app, env) {
+    app.use(primeRequestContext)
+    app.use(attachLocals)
+    app.use(lastResortErrorHandler)
+    app.use(express.static(join(__dirname, "..", "public"), { maxAge: 86400000 }))
+}
+
+module.exports = mountMiddleware
